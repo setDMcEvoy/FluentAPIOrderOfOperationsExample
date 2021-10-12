29ce0bd53492473823d073f45a39b977a22273e9
Daniel Mcevoy <Daniel.Mcevoy@DriveTime.com>
2021-10-12 08:45:56 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Common;
+
+namespace FluentAPIOrderOfOperationsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Creates an order in your online cart at your store
+            var buyStuff = Order.CreateOrder()
+                .AtStore("StoreName")
+                .ForTime(DateTime.Now)
+                .ForUser("moi")
+                .WithPassword("SuperSecretInPlainText")
+                .AddItem("Junk Food")
+                .AddItem("Lettuce")
+                .Submit();
+
+            //Order.CreateOrder().AtStore("").ForTime("").ForUser("").WithPassword("").
+        }
+    }
+}
